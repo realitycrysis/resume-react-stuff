641925cb494b63bdbe47752a94b5d72fd5a2f5e2
Scott Thomas <realitycrysis@gmail.com>
2019-12-17 15:02:58 -0600

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"https://wallpaperaccess.com/full/262810.jpg",
+    "name": "Scott Petrick",
+    "role": "Full Stack Developer",
+    "linkedinId":"Linkedin Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I enjoy front end and back end, server side devlopment. More specifically, I enjoy working with Express framework and the React library. I also like to learn about new technologies, read technical articles, or simply make music in my spare time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/realitycrysis",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am a recent graduate from Digital Crafts coding bootcamp. I am a full stack web developer, currently diving deeper into the React javascript library. I believe that to be successful in life, one needs to be obsessive with their dreams and be willing to make the sacrifices necessary to achieve them.",
+    "address":"Houston, Texas",
+    "website":"https://github.com/realitycrysis",
+    "education":[
+      // {
+      //   "UniversityName":"Digital Crafs",
+      //   "specialization":" specialization",
+      //   "MonthOfPassing":"December",
+      //   "YearOfPassing":"2019",
+      //   "Achievements":" Achievements"
+      // },
+      {
+        "UniversityName":"Houston Community College",
+        "specialization":"Computer engineering",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2018",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Engenuity Inc.",
+        "specialization":"Intern",
+        "MonthOfLeaving":"January",
+        "YearOfLeaving":"2020",
+        "Achievements":"Designed a full stack technical support application for oil company"
+      },
+      {
+        "CompanyName":"Digital Crafts",
+        "specialization":"Developer",
+        "MonthOfLeaving":"December",
+        "YearOfLeaving":"2019",
+        "Achievements":"Designed several front end, back end, and full stack applications using HTML, CSS, Javascript, Node.js, Express, and the React libary"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
